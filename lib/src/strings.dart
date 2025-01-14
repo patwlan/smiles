afc3a6060a87e0f0b4b14badda4b56e4d3744b6a
patwlan <163450632+patwlan@users.noreply.github.com>
2025-01-14 09:28:27 +0100

    feat: add substringAfter

--- lib/src/strings.dart
+++ lib/src/strings.dart
@@ -1,16 +1,19 @@
 //
 // String extensions
 //
-extension Strings on String? {
+extension Strings on String {
   // Counts the number of occurrences of a search term in a string.
-  count(String searchTerm) {
-    if (this == null) return 0;
-    return RegExp(searchTerm, caseSensitive: false).allMatches(this!).length;
+  int count(String searchTerm) {
+    return RegExp(searchTerm, caseSensitive: false).allMatches(this).length;
   }
 
   // Returns the string if it is not empty, otherwise returns null.
-  nullIfEmpty() {
-    if (this == null) return null;
-    return this!.isEmpty ? null : this;
+  String? nullIfEmpty() {
+    return isEmpty ? null : this;
+  }
+
+  // Returns the substring after the first occurrence of delimiter.
+  String? substringAfter(Pattern delimiter) {
+    return substring(indexOf(delimiter) + 1);
   }
 }
